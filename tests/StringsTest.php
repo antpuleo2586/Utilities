f01c7104f7979ec2bccb3870fb52981601f2f69b
Antonio <antonio.l.puleo@gmail.com>
2017-06-02 16:52:15 +0100

    added Strings class, added implodeMulti to Arrays class, added unit tests for both

---
+++ tests/StringsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests;
+
+use Ant\Utilities\Strings;
+use PHPUnit\Framework\TestCase;
+
+class StringsTest extends TestCase
+{
+    /**
+     * Testing the Strings::containsEmoji() function
+     */
+    public function testContainsEmoji()
+    {
+        $this->assertTrue(Strings::containsEmoji('🌐'));
+
+        $this->assertFalse(Strings::containsEmoji('a'));
+        $this->assertFalse(Strings::containsEmoji('ß'));
+        $this->assertFalse(Strings::containsEmoji('漢'));
+        $this->assertFalse(Strings::containsEmoji('1'));
+        $this->assertFalse(Strings::containsEmoji('@'));
+        $this->assertFalse(Strings::containsEmoji('æ'));
+    }
+
+    /**
+     * Testing the Strings::containsHtml() function
+     */
+    public function testContainsHtml()
+    {
+        $this->assertTrue(Strings::containsHtml('<p><p/>'));
+
+        $this->assertFalse(Strings::containsHtml('a'));
+        $this->assertFalse(Strings::containsHtml('1'));
+    }
+}
